c3b112fce838693203824779be084747c58b03ec
Elton Mozart <elton.mozart@gmail.com>
2020-04-14 11:32:01 -0300

    Removido console.log

--- src/app.js
+++ src/app.js
@@ -65,7 +65,6 @@ app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params
 
   const index = repositories.findIndex(repo => repo.id === id)
-  console.log(`INDEX: ${index}`)
 
   if(index === -1){
     return response.status(400).json({ error: "Repository doesn't exist"})
